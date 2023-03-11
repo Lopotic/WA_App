1998f120002d10c6ca037031edb23e9073649e18
Lopotic <martinfort88@gmail.com>
2023-03-11 16:00:40 +0100

    login

--- MobileApp/Screens/homeScreen.js
+++ MobileApp/Screens/homeScreen.js
@@ -1,13 +1,16 @@
 import * as React from 'react';
 import { Text, View, StyleSheet, Image ,ImageBackground} from 'react-native';
 import colors from '../components/colors';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
 
 export default function HomeScreen({navigation}){
     return(
       <ImageBackground  source={require('../assets/hxgn.png')} style={{width: '100%', height: '100%'}}>
         <View style = { styles.container }>
           <View style = { styles.header }>
-            <Text>Mobilní aplikace</Text>
+            <Text>Mobilní aplikace</Text> 
+            <MaterialCommunityIcons title='android'/>
           </View>
           <View style = {styles.jmena}>
             <Text>Martin Fořt</Text>
--- MobileApp/Screens/loginScreen.js
+++ MobileApp/Screens/loginScreen.js
@@ -1,19 +1,32 @@
 import React from 'react';
-import { Text, StyleSheet, TextInput, View } from 'react-native';
+import { Text, StyleSheet, TextInput, View, Alert } from 'react-native';
+import { useState, useEffect } from 'react';
+
 import { SafeAreaView } from 'react-native-safe-area-context';
+
 import MyButton from '../components/button';
 
-
 const LoginScreen = ({navigation}) => {
+    const [login, setLogin] = useState('');
+    const [password, setPassword] = useState('');
+  
+    const handleLogin = () => {
+      if (login === 'hlavka123' && password === '1234') {
+        navigation.navigate('the_rest', { screen: 'Home' });
+      } else {
+        Alert.alert('Špatné heslo nebo jméno, prosím zkuste znovu');
+      }
+    };
+
     return (
         <SafeAreaView style={styles.temp}>
             <View style={styles.temp2}>
                 <Text>Login</Text>
-                <TextInput id='login' placeholder='login'/>
+                <TextInput id='login' placeholder='login' style={styles.input} value={login} onChangeText={setLogin}/>
                 <Text>Password</Text>
-                <TextInput id='password' placeholder='password'/>
+                <TextInput id='password' placeholder='password' style={styles.input} secureTextEntry={true} value={password} onChangeText={setPassword}/>
             </View>
-                <MyButton title='Login' onPress={() => navigation.navigate('the_rest', {screen: 'Home'})}/>
+                <MyButton title='Login' onPress={handleLogin}/>
         </SafeAreaView>
     );
 }
@@ -25,6 +38,14 @@ const styles = StyleSheet.create({
     temp2: {
         marginBottom: 22,
         marginTop: 56,
-    }
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: '#ccc',
+        borderRadius: 4,
+        padding: 10,
+        marginBottom: 16,
+        width: 250,
+      },
 })
 export default LoginScreen;
